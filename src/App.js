0bf1893818e2bf25f2777f4c36dbd19e6084d7cb
Aldroc <riptide1112@gmail.com>
2020-07-19 10:43:22 +0530

    jumbotron

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Navigationbar from './components/navbar-component';
+import Jumbo from './components/jumbotron-component'
 
 function App() {
   return (
     <Router>
       <Navigationbar />
+      <Jumbo />
     </Router>
   );
 }
---
+++ src/components/jumbotron-component.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Jumbotron, Button } from 'reactstrap';
+import './css/jumbotron-component.css';
+
+export default class Jumbo extends Component {
+    constructor(props){
+        super(props);
+
+        this.onClick = this.onClick.bind(this);
+    }
+
+    onClick() {
+        console.log('onClick Event Generated')
+    }
+
+    render(){
+        return(
+            <Jumbotron className = "header">
+                <div className = "container">
+                    <div className = "row align-items justify-content-center align-items-center">
+                        <div className = "col-12 col-sm-4">
+                            <h2>Sample Logo</h2>
+                        </div>
+                        <div className = "col-auto">
+                            <h4>Sample text to promote product/company</h4><br />
+                            <div className = "row align-items-center justify-content-center">
+                                <div className = "col-auto">
+                                    <Button className = "button col-offset-6" onClick = { this.onClick }>Check Out Our Product!</Button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>    
+            </Jumbotron>    
+        );    
+    }        
+}
