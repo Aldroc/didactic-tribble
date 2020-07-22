40c50fb33fad93dd5dff9a936f647cee2e9cfd29
Aldroc <riptide1112@gmail.com>
2020-07-22 20:48:22 +0530

    carousel_with_sample_img_implemented

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Navigationbar from './components/navbar-component';
-import Jumbo from './components/jumbotron-component'
+import Jumbo from './components/jumbotron-component';
+import Home from './components/home-component';
 
 function App() {
   return (
     <Router>
       <Navigationbar />
       <Jumbo />
+      <Route path = "/" exact component = { Home } /> 
     </Router>
   );
 }
